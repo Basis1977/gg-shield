4577cf32f7b2e10692312dc9274e120cfdb0d136
Basis1977 <55097424+Basis1977@users.noreply.github.com>
2022-10-12 21:56:25 +0200

    Update and rename config.py to config

--- config.py
+++
@@ -1,8 +0,0 @@
-[default]
-    
-    aws_access_key_id = akdflkdjfodsfpdsfojsdof
-    aws_secret_access_key = klaldkjfodijrlekroeijro
-    token":"NMIfyYncKcRALEXAMPLE",
-    type":"mycompany_api_token",
-    url":"https://github.com/octocat/Hello-World/blob/12345600b9cbe38a219f39a9941c9319b600c002/foo/bar.txt",
-    source":"content"
